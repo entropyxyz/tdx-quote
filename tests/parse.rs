3538cac8b83fcb1220c2f414ccc0ab1057073eca
peg <ameba23@systemli.org>
2024-09-08 17:24:09 +0200

    WIP verify PCK signature in certification data

--- tests/parse.rs
+++ tests/parse.rs
@@ -1,14 +1,26 @@
 use std::io::Read;
-use tdx_quote::{Quote, QuoteParseError};
+use tdx_quote::{Quote, QuoteParseError, Verifier, VerifyingKey};
 
 #[test]
 fn test_parse() {
-    let mut file = std::fs::File::open("tests/v4_quote.dat").unwrap();
+    let mut file = std::fs::File::open("tests/another_quote.dat").unwrap();
     let mut input = Vec::new();
     file.read_to_end(&mut input).unwrap();
     let quote = Quote::from_bytes(&input).unwrap();
     assert_eq!(quote.header.version, 4);
-    println!("{:?}", quote);
+    println!("{:?}", quote.certification_data);
+    let qe_report_certification_data = quote.qe_report_certification_data().unwrap();
+
+    let attestation_key = [0u8; 64];
+    // if attestation key is only 64 bytes, add the flag
+    let attestation_key = [&[4], &attestation_key[..]].concat(); // 0x04 means uncompressed
+    let attestation_key = VerifyingKey::from_sec1_bytes(&attestation_key).unwrap();
+    attestation_key
+        .verify(
+            &qe_report_certification_data.qe_report[..],
+            &qe_report_certification_data.signature,
+        )
+        .unwrap();
 
     // Fails to verify signature if the input data changes
     input[49] += 1;
